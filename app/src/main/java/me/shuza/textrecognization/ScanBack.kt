c0fda06b23f504fb79b7a8ba5e972ca712cf1abb
taha_jadid <taha.jadid@inwi.ma>
2021-12-17 12:08:14 +0100

    Add Constraint layout

--- app/src/main/java/me/shuza/textrecognization/ScanBack.kt
+++ app/src/main/java/me/shuza/textrecognization/ScanBack.kt
@@ -5,10 +5,9 @@ import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.Button
@@ -31,21 +30,21 @@ class ScanBack : AppCompatActivity() {
     private lateinit var surface_camera_preview: SurfaceView
     val stringBuilder = StringBuilder()
     private val PERMISSION_REQUEST_CAMERA = 100
-    private lateinit var button: Button;
-    private lateinit var buttonFront: Button;
-    private lateinit var buttonBack: Button;
-    private lateinit var buttonNext: Button;
+    private lateinit var button: Button
+    private lateinit var buttonFront: Button
+    private lateinit var buttonBack: Button
+    private lateinit var buttonNext: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_back)
-        tv_result=findViewById(R.id.tv_result)
-        button=findViewById(R.id.button)
-        buttonBack=findViewById(R.id.buttonBack)
-        buttonNext=findViewById(R.id.buttonNext)
+        tv_result = findViewById(R.id.tv_result)
+        button = findViewById(R.id.button)
+        buttonBack = findViewById(R.id.buttonBack)
+        buttonNext = findViewById(R.id.buttonNext)
         // buttonNext=findViewById(R.id.buttonNext)
         /*  buttonBack=findViewById(R.id.buttonBack)
           buttonSim=findViewById(R.id.buttonSim)*/
-        surface_camera_preview=findViewById(R.id.surface_camera_preview)
+        surface_camera_preview = findViewById(R.id.surface_camera_preview)
         startCameraSource()
         buttonBack.setOnClickListener {
             val builder = AlertDialog.Builder(this)
@@ -53,13 +52,17 @@ class ScanBack : AppCompatActivity() {
             builder.setMessage(stringBuilder.toString())
 
             builder.setPositiveButton(android.R.string.yes) { dialog, which ->
-                Toast.makeText(applicationContext,
-                    android.R.string.yes, Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    android.R.string.yes, Toast.LENGTH_SHORT
+                ).show()
             }
 
             builder.setNegativeButton(android.R.string.no) { dialog, which ->
-                Toast.makeText(applicationContext,
-                    android.R.string.no, Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    android.R.string.no, Toast.LENGTH_SHORT
+                ).show()
             }
 
             builder.show()
@@ -71,11 +74,11 @@ class ScanBack : AppCompatActivity() {
         }
         button.setOnClickListener {
 
-            //startCameraSource()
+            // startCameraSource()
             textRecognizer.setProcessor(object : Detector.Processor<TextBlock> {
                 override fun release() {}
 
-                override fun  receiveDetections(detections: Detector.Detections<TextBlock>) {
+                override fun receiveDetections(detections: Detector.Detections<TextBlock>) {
                     val items = detections.detectedItems
 
                     if (items.size() <= 0) {
@@ -84,113 +87,97 @@ class ScanBack : AppCompatActivity() {
 
                     tv_result.post {
 
-
                         stringBuilder.setLength(0)
                         for (i in 0 until items.size()) {
                             val item = items.valueAt(i)
-                            //Pattern of Sexe
-                            if(Pattern.matches("Sexe.*(M|F)",item.value)){
+                            // Pattern of Sexe
+                            if (Pattern.matches("Sexe.*(M|F)", item.value)) {
 
-                                stringBuilder.append(item.value.toString());
+                                stringBuilder.append(item.value.toString())
                             }
-                            if(Pattern.matches("M|F",item.value)){
+                            if (Pattern.matches("M|F", item.value)) {
 
-
-                                stringBuilder.append("Sexe : ",item.value.toString().toUpperCase()+"\n");
+                                stringBuilder.append("Sexe : ", item.value.toString().toUpperCase() + "\n")
                             }
-                            //Pattern of address
+                            // Pattern of address
 
-                            if(Pattern.matches("Adresse.*",item.value)){
-                                    stringBuilder.append(item.value.toString().toUpperCase()+"\n");
+                            if (Pattern.matches("Adresse.*", item.value)) {
+                                stringBuilder.append(item.value.toString().toUpperCase() + "\n")
                             }
-
                         }
 
-                      //  tv_result.text = stringBuilder.toString()
-
-
-
-
+                        //  tv_result.text = stringBuilder.toString()
                     }
                 }
             })
-
-
         }
-
-
-
     }
 
+    private fun startCameraSource() {
 
-private fun startCameraSource() {
+        //  Create text Recognizer
+        textRecognizer = TextRecognizer.Builder(this).build()
 
-    //  Create text Recognizer
-    textRecognizer = TextRecognizer.Builder(this).build()
-
-    if (!textRecognizer.isOperational) {
-        toast("Dependencies are not loaded yet...please try after few moment!!")
-        Logger.d("Dependencies are downloading....try after few moment")
-        return
-    }
-
-    //  Init camera source to use high resolution and auto focus
-    mCameraSource = CameraSource.Builder(applicationContext, textRecognizer)
-        .setFacing(CameraSource.CAMERA_FACING_BACK)
-        .setRequestedPreviewSize(1280, 1024)
-        .setAutoFocusEnabled(true)
-        .setRequestedFps(2.0f)
-        .build()
-
-    surface_camera_preview.holder.addCallback(object : SurfaceHolder.Callback {
-        override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
-
+        if (!textRecognizer.isOperational) {
+            toast("Dependencies are not loaded yet...please try after few moment!!")
+            Logger.d("Dependencies are downloading....try after few moment")
+            return
         }
 
-        override fun surfaceDestroyed(p0: SurfaceHolder?) {
-            mCameraSource.stop()
-        }
+        //  Init camera source to use high resolution and auto focus
+        mCameraSource = CameraSource.Builder(applicationContext, textRecognizer)
+            .setFacing(CameraSource.CAMERA_FACING_BACK)
+            .setRequestedPreviewSize(1280, 1024)
+            .setAutoFocusEnabled(true)
+            .setRequestedFps(2.0f)
+            .build()
 
-        @SuppressLint("MissingPermission")
-        override fun surfaceCreated(p0: SurfaceHolder?) {
-            try {
-                if (isCameraPermissionGranted()) {
-                    mCameraSource.start(surface_camera_preview.holder)
-                } else {
-                    requestForPermission()
+        surface_camera_preview.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun surfaceDestroyed(p0: SurfaceHolder?) {
+                mCameraSource.stop()
+            }
+
+            @SuppressLint("MissingPermission")
+            override fun surfaceCreated(p0: SurfaceHolder?) {
+                try {
+                    if (isCameraPermissionGranted()) {
+                        mCameraSource.start(surface_camera_preview.holder)
+                    } else {
+                        requestForPermission()
+                    }
+                } catch (e: Exception) {
+                    toast("Error:  ${e.message}")
                 }
-            } catch (e: Exception) {
-                toast("Error:  ${e.message}")
+            }
+        })
+    }
+
+    fun isCameraPermissionGranted(): Boolean {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+            PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestForPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode != PERMISSION_REQUEST_CAMERA) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (isCameraPermissionGranted()) {
+                mCameraSource.start(surface_camera_preview.holder)
+            } else {
+                toast("Permission need to grant")
+                finish()
             }
         }
-    })
-
-
-}
-
-fun isCameraPermissionGranted(): Boolean {
-    return ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
-            PackageManager.PERMISSION_GRANTED
-}
-
-private fun requestForPermission() {
-    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA)
-}
-
-@SuppressLint("MissingPermission")
-override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-    if (requestCode != PERMISSION_REQUEST_CAMERA) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        return
     }
-
-    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-        if (isCameraPermissionGranted()) {
-            mCameraSource.start(surface_camera_preview.holder)
-        } else {
-            toast("Permission need to grant")
-            finish()
-        }
-    }
-}
 }
