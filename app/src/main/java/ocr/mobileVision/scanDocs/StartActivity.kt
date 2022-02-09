37d3e2e03fc0e1712660c61e9174bcb52ef3a279
taha_jadid <taha.jadid@inwi.ma>
2022-02-09 18:18:03 +0100

    Add the result layout

--- app/src/main/java/ocr/mobileVision/scanDocs/StartActivity.kt
+++ app/src/main/java/ocr/mobileVision/scanDocs/StartActivity.kt
@@ -1,5 +1,6 @@
 package ocr.mobileVision.scanDocs
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -7,6 +8,9 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.ActionBar
+
 
 class StartActivity : AppCompatActivity() {
 
